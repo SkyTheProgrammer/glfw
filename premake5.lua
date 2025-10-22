1f59b84f7cc1b1a5dda1d8eca306e152231bbce5
Sky <34356003+SkyTheProgrammer@users.noreply.github.com>
2025-10-22 10:55:06 -0600

    fixed some missing modules in premake

--- premake5.lua
+++ premake5.lua
@@ -14,8 +14,13 @@ project "GLFW"
             "src/init.c",
             "src/input.c",
             "src/monitor.c",
+            "src/null_init.c",
+            "src/null_joystick.c",
+            "src/null_monitor.c",
+            "src/null_window.c",
+            "src/platform.c",
             "src/vulkan.c",
-            "src/window.c"
+            "src/window.c",
         }
 
         filter "system:windows"
@@ -26,6 +31,7 @@ project "GLFW"
             {
                 "src/win32_init.c",
                 "src/win32_joystick.c",
+                "src/win32_module.c",
                 "src/win32_monitor.c",
                 "src/win32_time.c",
                 "src/win32_thread.c",
@@ -34,3 +40,22 @@ project "GLFW"
                 "src/egl_context.c",
                 "src/osmesa_context.c"
             }
+
+            defines
+            {
+                "_GLFW_WIN32",
+                "_CRT_SECRUE_NO_WARNINGS"
+            }
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+            
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "speed"    
+
+        filter "configurations:Dist"
+            runtime "Release"
+            optimize "speed"
+            symbols "off"
